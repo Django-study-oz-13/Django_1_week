da5f9c2337692ab7073b5cab1ff9fddf53a36fdf
TeaCat-Neya <TeaCat0416@gmail.com>
2025-09-18 21:52:35 +0900

    ✨ Todo 추가 뷰함수 생성
    def todo_post ...

--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Todo
+from .forms import TodoForm
 
-# Create your views here.
+def todo_post(request):
+    if request.method == 'POST':
+        form = TodoForm(request.POST)
+        if form.is_valid():
+            todo = form.save(commit=False)
+            todo.save()
+            return redirect('todo_list')
+    else:
+        form = TodoForm()
+    return render(request, 'todo/todo_post.html', {'form':form})
