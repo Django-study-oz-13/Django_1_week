22f0827fc3b80f612d114d60365806f0c0a8a785
LuSiEDa <wltlrtmdgus@naver.com>
2025-09-23 21:54:13 +0900

    merge완료

--- todo/form.py
+++
@@ -1,8 +0,0 @@
-from todo.models import Todo
-from django import forms
-
-
-class TodoForm(forms.ModelForm):
-    class Meta:
-        model = Todo
-        fields = ['title', 'description', 'important']
