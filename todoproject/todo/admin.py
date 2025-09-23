770a888c2fa50d88393e2c2fe508718175474e11
LuSiEDa <wltlrtmdgus@naver.com>
2025-09-23 21:29:49 +0900

    내 변경사항 저장
     Conflicts:
    	config/__pycache__/__init__.cpython-313.pyc
    	config/__pycache__/settings.cpython-313.pyc
    	todo/form.py
    	todo/models.py
    	todo/views.py
    	todoproject/config/urls.py
    	todoproject/todo/admin.py
    
    qw

--- todoproject/todo/admin.py
+++ todoproject/todo/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from .models import Todo
 
-# Register your models here.
-admin.site.register(Todo)
+from todo.models import Todo
+
+
+@admin.register(Todo)
+class TodoAdmin(admin.ModelAdmin):
+    pass
