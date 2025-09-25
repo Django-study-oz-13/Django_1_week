8cf74d514e53760d3fbec458c3c6a2c5ebb084b7
summmer5 <jisun131200@gmail.com>
2025-09-25 20:32:14 +0900

    done_list.html 내용 작성

--- todoproject/todo/admin.py
+++ todoproject/todo/admin.py
@@ -1,5 +1,13 @@
 from django.contrib import admin
+<<<<<<< Updated upstream:todoproject/todo/admin.py
 from .models import Todo
 
 # Register your models here.
 admin.site.register(Todo)
+=======
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+]
+>>>>>>> Stashed changes:todo/admin.py
